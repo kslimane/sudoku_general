3333b7f748a3219a701aa720cc03ce8ed7972a29
kslimane <kslimane42@gmail.com>
2019-07-23 17:30:27 +0200

    Ajouter benchmarking et fichier de sudoku.

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -14,15 +14,24 @@ namespace Sudoku_General
 
             var types = AppDomain.CurrentDomain.GetAssemblies()
              .SelectMany(s => s.GetTypes())
-                .Where(p => typeof(SudokuLib.ISudokuSolver).IsAssignableFrom(p));
+             .Where(p => typeof(SudokuLib.ISudokuSolver).IsAssignableFrom(p));
+
             foreach (Type item in types)
             {
+                Sudoku sud = new Sudoku();
                 ISudokuSolver localSolver = (ISudokuSolver)Activator.CreateInstance(item);
-                //localSolver.Solve()
 
+                var sud_one = sud.ParseFile(@"C:\Users\K\Documents\Work\CSharp\Sudoku_General\Benchmark\sudoku_files\Easy.txt")[0];
+
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                localSolver.Solve(sud_one);
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+
+                Console.WriteLine(item.Name);
+                Console.WriteLine(elapsedMs);
+                Console.ReadKey();
             }
-
-
         }
     }
 }
