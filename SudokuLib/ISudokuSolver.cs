5ff196c85fd5bab2574a9dbfdcc0ce52fa200678
kslimane <kslimane42@gmail.com>
2019-07-23 14:47:03 +0200

    refonte structure dossiers

---
+++ SudokuLib/ISudokuSolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLib
+{
+    public interface ISudokuSolver
+    {
+        Sudoku Solve(Sudoku sudoku);
+    }
+
+    public class SudokuSolver : ISudokuSolver
+    {
+        public Sudoku Solve(Sudoku s)
+        {
+            return new Sudoku();
+        }
+    }
+}
+
